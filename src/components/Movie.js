c9d4718b21f347e713c75cbf80e76acc417ace8b
Comet105 <taegyeon512@naver.com>
2022-02-18 21:45:02 +0900

    using styled components

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,20 +1,86 @@
 import PropTypes from "prop-types";
+import { useState } from "react";
 import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+const Container = styled.div`
+  display: flex;
+  width: auto;
+`;
+
+const MovieBox = styled.div`
+  display: flex;
+  background-color: white;
+  margin-bottom: 70px;
+  font-weight: 2rem;
+  padding: 2rem;
+  border-radius: 5px;
+  width: auto;
+  border: 1px solid;
+  color: #adaeb9;
+`;
+
+const MainImg = styled.img``;
+
+const Ul = styled.ul`
+  display: flex;
+`;
+
+const Li = styled.li`
+  list-style: none;
+  padding-left: 0px;
+  margin-left: 5px;
+`;
+
+const Div = styled.div`
+  margin-left: 3rem;
+`;
+
+const Button = styled.button`
+  background-color: white;
+  border: 0;
+`;
+
+const Description = styled.div`
+  max-width: 20rem;
+`;
 
 function Movie({ id, coverImg, title, summary, genres }) {
+  const [movieBook, setmovieBook] = useState(true);
+  const onClick = () => {
+    setmovieBook(!movieBook);
+  };
   return (
-    <div>
-      <img src={coverImg} alt={title} />
-      <h2>
-        <Link to={`/movie/${id}`}>{title}</Link>
-      </h2>
-      <p>{summary}</p>
-      <ul>
-        {genres.map((g) => (
-          <li key={g}>{g}</li>
-        ))}
-      </ul>
-    </div>
+    <Container>
+      <MovieBox>
+        {movieBook ? (
+          <div>
+            <Button onClick={onClick}>
+              <MainImg src={coverImg} />
+            </Button>
+          </div>
+        ) : (
+          <>
+            <Button onClick={onClick}>
+              <MainImg src={coverImg} />
+            </Button>
+            <Div>
+              <h2>
+                <Link to={`/movie/${id}`}>{title}</Link>
+              </h2>
+              <Ul>
+                {genres.map((g) => (
+                  <Li key={g}>{g}</Li>
+                ))}
+              </Ul>
+              <Description>
+                {summary.length > 235 ? `${summary.slice(0, 235)}...` : summary}
+              </Description>
+            </Div>
+          </>
+        )}
+      </MovieBox>
+    </Container>
   );
 }
 
--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,18 +1,36 @@
 import { useParams } from "react-router-dom";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 function Detail() {
   const { id } = useParams();
+  const [loading, setLoading] = useState(true);
+  const [details, setDetails] = useState([]);
+
   const getMovie = async () => {
     const json = await (
       await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
     ).json();
     console.log(json);
+    setDetails(json.data.movie);
+    setLoading(false);
   };
+
   useEffect(() => {
     getMovie();
   }, []);
-  return <h1>detail</h1>;
+
+  return (
+    <div>
+      <h1>{loading ? "Waiting..." : `${details.title}`}</h1>
+      {loading ? (
+        <div></div>
+      ) : (
+        <div>
+          <img src={details.medium_cover_image} />
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default Detail;
